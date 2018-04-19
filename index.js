dfe570f4d2689a9134599cc345186169409c4f06
yuhijackman <yuhi.j.adams@gmail.com>
2018-04-19 23:49:56 +0900

    Improve

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // -----------------------------------------------------------------------------
 // モジュールのインポート!
-const server = require("express")();
+var express = require('express');
+const server = express();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 // -----------------------------------------------------------------------------
@@ -25,32 +26,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
-app.post('/webhook', function(req, res, next){
-    res.status(200).end();
-    for (var event of req.body.events){
-        if (event.type == 'message' && event.message.text == 'ハロー'){
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + line_config
-            }
-            var body = {
-                replyToken: event.replyToken,
-                messages: [{
-                    type: 'text',
-                    text: 'こんにちは'
-                }]
-            }
-            var url = 'https://api.line.me/v2/bot/message/reply';
-            request({
-                url: url,
-                method: 'POST',
-                headers: headers,
-                body: body,
-                json: true
-            });
-        }
-    }
-});
 // APIコールのためのクライアントインスタンスを作成
 const bot = new line.Client(line_config);
 
