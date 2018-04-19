9322525a25bad6d0d734039e046e5c06e4dc00c4
yuhijackman <yuhi.j.adams@gmail.com>
2018-04-19 23:37:06 +0900

    Improve

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-// -----------------------------------------------------------------------------
-// モジュールのインポート!
-const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+// // -----------------------------------------------------------------------------
+// // モジュールのインポート!
+// const server = require("express")();
+// const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
-const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-};
+// // -----------------------------------------------------------------------------
+// // パラメータ設定
+// const line_config = {
+//     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+//     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+// };
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-server.listen(process.env.PORT || 3000);
+// // -----------------------------------------------------------------------------
+// // Webサーバー設定
+// server.listen(process.env.PORT || 3000);
 
 
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log(req.body);
-});
+// // -----------------------------------------------------------------------------
+// // ルーター設定
+// server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+//     res.sendStatus(200);
+//     console.log(req.body);
+// });
 
-//省略
+// //省略
 
-// APIコールのためのクライアントインスタンスを作成
-const bot = new line.Client(line_config);
+// // APIコールのためのクライアントインスタンスを作成
+// const bot = new line.Client(line_config);
 
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    // 先行してLINE側にステータスコード200でレスポンスする。
-    res.sendStatus(200);
-    console.log('Diagon Alley Boy');
-    // すべてのイベント処理のプロミスを格納する配列。
-    let events_processed = [];
+// // -----------------------------------------------------------------------------
+// // ルーター設定
+// server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+//     // 先行してLINE側にステータスコード200でレスポンスする。
+//     res.sendStatus(200);
+//     console.log('Diagon Alley Boy');
+//     // すべてのイベント処理のプロミスを格納する配列。
+//     let events_processed = [];
 
-    // イベントオブジェクトを順次処理。
-    req.body.events.forEach((event) => {
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
-            console.log('Hagrid');
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "A"){
-                console.log('Professor McGonagall');
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
-            } else {
-                console.log('Albus Dumbledore');
-            }
-        } else {
-            console.log('Harry Potter');
-        }
-    });
+//     // イベントオブジェクトを順次処理。
+//     req.body.events.forEach((event) => {
+//         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+//         if (event.type == "message" && event.message.type == "text"){
+//             console.log('Hagrid');
+//             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+//             if (event.message.text == "A"){
+//                 console.log('Professor McGonagall');
+//                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+//                 events_processed.push(bot.replyMessage(event.replyToken, {
+//                     type: "text",
+//                     text: "これはこれは"
+//                 }));
+//             } else {
+//                 console.log('Albus Dumbledore');
+//             }
+//         } else {
+//             console.log('Harry Potter');
+//         }
+//     });
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
-});
+//     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+//     Promise.all(events_processed).then(
+//         (response) => {
+//             console.log(`${response.length} event(s) processed.`);
+//         }
+//     );
+// });
